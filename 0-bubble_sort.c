71405dc3b5b66bac5e6777141b1610c9c265b9b6
Omar Elsendiony <omar.sendiony@gmail.com>
2023-12-16 01:02:25 +0200

    fixed betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,28 +2,35 @@
 
 
 
-
+/**
+ * bubble_sort - Returns the number of elements
+ * @array: A pointer to the head of the listint_t list.
+ * @size: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the listint_t list.
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    int temp, thereisSwap = 1;
+	size_t i;
+	int temp, thereisSwap = 1;
 
-    if (size < 2) return;
-    while(thereisSwap)
-    {
-        i = 0;
-        thereisSwap = 0;
-        for (; i < size - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                /* swap */
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                print_array(array, size);
-                thereisSwap = 1;
-            }
-        }
-    }
+	if (size < 2)
+		return;
+	while (thereisSwap)
+	{
+		i = 0;
+		thereisSwap = 0;
+		for (; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				/* swap */
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+				thereisSwap = 1;
+			}
+		}
+	}
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,48 @@
 #include "sort.h"
 
 
+/**
+ * insertion_sort_list - Returns the number of elements
+ * @list: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the listint_t list.
+ */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *HEAD = *list, *node, *savedNode;
-    node = HEAD->next;
-    while(node != NULL)
-    {
-        savedNode = node->next;
-        if (node->prev->n > node->n)
-        {
-            while(node->prev != NULL)
-            {
-                if (node->prev->n > node->n)
-                {
-                    node->prev->next = node->next;
-                    if (node->next != NULL)
-                        node->next->prev = node->prev;
-                    node->next = node->prev;
-                    node->prev = node->prev->prev;
-                    node->next->prev = node;
-                    /* check for nullptr here*/
-                    if (node->prev)
-                        node->prev->next = node;
-                    else
-                    {
-                        HEAD = node;
-                    }
-                    print_list(HEAD);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-        node = savedNode;
-    }
-    *list = HEAD;
+	listint_t *HEAD = *list, *node, *savedNode;
+
+	node = HEAD->next;
+	while (node != NULL)
+	{
+		savedNode = node->next;
+		if (node->prev->n > node->n)
+		{
+			while (node->prev != NULL)
+			{
+				if (node->prev->n > node->n)
+				{
+					node->prev->next = node->next;
+					if (node->next != NULL)
+						node->next->prev = node->prev;
+					node->next = node->prev;
+					node->prev = node->prev->prev;
+					node->next->prev = node;
+					/* check for nullptr here*/
+					if (node->prev)
+						node->prev->next = node;
+					else
+					{
+						HEAD = node;
+					}
+					print_list(HEAD);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		node = savedNode;
+	}
+	*list = HEAD;
 }
